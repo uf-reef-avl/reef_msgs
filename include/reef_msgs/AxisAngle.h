773d62deaf823a5c6167e5e9a2bbd5691bdcf932
paul.buzaud <paul.buzaud@ufl.edu>
2020-10-19 15:17:41 -0500

    [DEV] Develop AxisAngle conversions functions

--- include/reef_msgs/AxisAngle.h
+++ include/reef_msgs/AxisAngle.h
@@ -12,37 +12,29 @@ namespace reef_msgs {
 
     public:
         AxisAngle(const double &_x, const double &_y, const double &_z, const double &_angle);
-
         AxisAngle(const Eigen::Matrix<double, 3, 1> &_axis, const double &_angle);
-
         AxisAngle(const Eigen::Matrix<double, 4, 1> &_axisAngle);
-
         AxisAngle(const AxisAngle &other);
 
         void setAxisAngle(const double &_x, const double &_y, const double &_z, const double &_angle);
-
         void setAxisAngle(const Eigen::Matrix<double, 3, 1> &_axis, const double &_angle);
-
         void setAxisAngle(const Eigen::Matrix<double, 4, 1> &_axisAngle);
-
         auto getAxisAngle() const -> const Eigen::Matrix<double, 4, 1> &;
-
         auto x() const -> double;
-
         void setX(const double &_x);
-
         auto y() const -> double;
-
         void setY(const double &_y);
-
         auto z() const -> double;
-
         void setZ(const double &_Z);
-
         auto angle() const -> double;
-
         void setAngle(const double &_angle);
 
+        auto normalized() -> Eigen::Matrix<double,4,1>;
+        auto toEulerAngle(const std::string &_eulerTransformation ="321") -> Eigen::Matrix<double,3,1>;
+        auto toRotationMatrix() -> Eigen::Matrix3d;
+        auto toQuaternion() -> Eigen::Matrix<double,4,1>;
+        auto toRodriguezParameter() -> Eigen::Matrix<double,3,1>;
+        auto toDCM() -> Eigen::Matrix3d;
         AxisAngle &operator=(const AxisAngle &_other);
 
         friend std::ostream &operator<<(std::ostream &os, const AxisAngle &_inst);
--- src/AxisAngle.cpp
+++ src/AxisAngle.cpp
@@ -2,6 +2,7 @@
 // Created by paul on 10/5/20.
 //
 #include "../include/reef_msgs/AxisAngle.h"
+#include "../include/reef_msgs/Quaternion.h"
 
 namespace reef_msgs {
 
@@ -72,8 +73,73 @@ namespace reef_msgs {
             : AngleRepresentationInterface(), m_axisAngle(_axisAngle) {
     }
 
-    AxisAngle::AxisAngle(const Eigen::Matrix<double, 3, 1> &_axis, const double &_angle) : AngleRepresentationInterface() {
+    AxisAngle::AxisAngle(const Eigen::Matrix<double, 3, 1> &_axis, const double &_angle)
+            : AngleRepresentationInterface() {
         setAxisAngle(_axis, _angle);
+    }
+
+    auto AxisAngle::normalized() -> Eigen::Matrix<double, 4, 1> {
+        Eigen::Matrix<double, 4, 1> q;
+        q(0, 0) = sqrt((m_axisAngle.transpose() * m_axisAngle)(0, 0));
+        q(1, 0) = m_axisAngle(0, 0) / q(0, 0);
+        q(2, 0) = m_axisAngle(1, 0) / q(0, 0);
+        q(3, 0) = m_axisAngle(2, 0) / q(0, 0);
+        return q;
+    }
+
+    auto AxisAngle::toEulerAngle(const std::string &_eulerTransformation) -> Eigen::Matrix<double, 3, 1> {
+        Eigen::Matrix<double, 3, 1> eulerAngle;
+        eulerAngle = reef_msgs::Quaternion(toQuaternion()).toEulerAngle(_eulerTransformation);
+        return eulerAngle;
+    }
+
+    auto AxisAngle::toRotationMatrix() -> Eigen::Matrix3d {
+        return toDCM().transpose();
+    }
+
+    auto AxisAngle::toQuaternion() -> Eigen::Matrix<double, 4, 1> {
+        Eigen::Matrix<double, 4, 1> q;
+        auto q1 = normalized();
+        auto sp = sin(q1(0, 0) / 2);
+        q(3, 0) = cos(q1(0, 0) / 2);
+        q(0, 0) = q1(1, 0) * sp;
+        q(1, 0) = q1(2, 0) * sp;
+        q(2, 0) = q1(3, 0) * sp;
+        return q;
+    }
+
+    auto AxisAngle::toRodriguezParameter() -> Eigen::Matrix<double, 3, 1> {
+        Eigen::Matrix<double, 3, 1> q1 = normalized();
+        auto tp = tan(q1(0, 0) / 2);
+        Eigen::Matrix<double, 3, 1> q;
+        q(0, 0) = q1(1, 0) * tp;
+        q(1, 0) = q1(2, 0) * tp;
+        q(2, 0) = q1(3, 0) * tp;
+
+        return q;
+    }
+
+    auto AxisAngle::toDCM() -> Eigen::Matrix3d {
+        auto q0 = sqrt(m_axisAngle.transpose() * m_axisAngle);
+        auto q1 = m_axisAngle(0, 0) / q0;
+        auto q2 = m_axisAngle(1, 0) / q0;
+        auto q3 = m_axisAngle(2, 0) / q0;
+
+        auto cp = cos(q0);
+        auto sp = sin(q0);
+        auto d1 = 1 - cp;
+        Eigen::Matrix<double, 3, 3> C;
+        C(0, 0) = q1 * q1 * d1 + cp;
+        C(0, 1) = q1 * q2 * d1 + q3 * sp;
+        C(0, 2) = q1 * q3 * d1 - q2 * sp;
+        C(1, 0) = q2 * q1 * d1 - q3 * sp;
+        C(1, 1) = q2 * q2 * d1 + cp;
+        C(1, 2) = q2 * q3 * d1 + q1 * sp;
+        C(2, 0) = q3 * q1 * d1 + q2 * sp;
+        C(2, 1) = q3 * q2 * d1 - q1 * sp;
+        C(2, 2) = q3 * q3 * d1 + cp;
+
+        return C;
     }
 
     AxisAngle &AxisAngle::operator=(const AxisAngle &_other) {
